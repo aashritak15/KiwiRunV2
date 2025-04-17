4dd81d40ba94282ed6903a4e6654bf47defe35bf
Infinity Chicken <8zsc2014@gmail.com>
2025-04-16 17:14:32 -0700

    ladybrown pseudocode

--- src/drivecode/ladybrown.cpp
+++ src/drivecode/ladybrown.cpp
@@ -15,4 +15,53 @@ void runLB() {
 
         pros::delay(10);
     }
+
+    double derivative = 0;
+    double prevError;
+
+    // while (true) {
+    //     if(std::abs(controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y)) > 50) { //TODO: tune
+    //         pidActive = true;
+    //         derivative = 0;
+    //     }
+
+    //     if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN)) {
+    //         if(!LB_BUTTON_PRESSED) {
+    //             pidActive = false;
+    //         } else {
+    //             LB_BUTTON_PRESSED = true;
+    //         }
+    //     }
+
+
+
+    //     if(pidActive == true) {
+    //         int target; //TODO: chnage
+    //         int current;
+    //         int kP;
+    //         int kD;
+
+    //         double error = target - current;
+    //         derivative = error - prevError;
+
+    //         double pTerm = kP * error;
+    //         double dTerm = kD * derivative;
+
+    //         ladyBrown.move_velocity(pTerm + dTerm);
+    //     } else {
+
+    //     }
+        
+    //     if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN)) {
+    //         if(!LB_BUTTON_PRESSED) {
+    //             pidActive = true;
+    //         } else {
+    //             LB_BUTTON_PRESSED = true;
+    //         }
+    //     }
+    //     double command = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y) / 127.0 * 100.0;
+    //     ladyBrown.move_velocity(command);
+
+    //     pros::delay(10);
+    // }
 }
