d58ee6cf0aca44f3397da97736818b625c8bc11b
lunence <mooncyjoyli@gmail.com>
2025-04-24 19:16:31 -0700

    red ring side

--- src/main.cpp
+++ src/main.cpp
@@ -22,13 +22,45 @@ void disabled() {}
 
 void competition_initialize() {}
 
+//temp, move to separate file later
+void redRingSide() {
+    chassis.setBrakeMode(pros::E_MOTOR_BRAKE_BRAKE);
+
+    //alliance
+    chassis.turnToHeading(-50, 500);
+    chassis.moveToPoint(-8.27, 4.23, 500);
+    chassis.waitUntilDone();
+    pidActive = true;
+    lbTarget = 180;
+    pros::delay(750);
+
+    //mogo
+    //TRASH MOGO MECH ROUTE
+    // chassis.moveToPoint(17.6, -15.8, 1000, {.forwards = false});
+    // chassis.turnToHeading(40, 750); //36.7
+    // chassis.moveToPoint(9, -30.2, 500, {.forwards = false});
+    // chassis.waitUntilDone();
+    // chassis.moveToPoint(3, -35.8, 500, {.forwards = false, .maxSpeed = 20});
+    // chassis.waitUntilDone();
+    // clampState = 1;
+    // pidActive = true;
+    // lbTarget = 2;
+    // pros::delay(5000);
+
+    //middle rings
+    chassis.turnToHeading(139, 500);
+    intakeState = 1;
+    chassis.moveToPose(30, -44, 90, 1000, {.lead = 0.5});
+    chassis.moveToPoint(40, -44, 1000);
+}
+
 
 void autonomous() {
     // chassis.setBrakeMode(pros::E_MOTOR_BRAKE_BRAKE);
     // kiwi::Path filePath(config, "/usd/path.json");
     // filePath.follow();
-
-    chassis.moveToPose(5, 30, 90, 4000); 
+  
+    redRingSide();
 }
 
 void opcontrol() {
@@ -36,25 +68,25 @@ void opcontrol() {
 
     while(true) {
 
-        if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_LEFT))  {
-            //chassis.turnToHeading(45, 2000);
-            chassis.moveToPoint(0, 24, 10000);
-            std::cout<<"SDGDSFSd";
-        }
-        if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_A)){
-            chassis.turnToHeading(90, 2000);}
+        // if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_LEFT))  {
+        //     //chassis.turnToHeading(45, 2000);
+        //     chassis.moveToPoint(0, 24, 10000);
+        //     std::cout<<"SDGDSFSd";
+        // }
+        // if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_A)){
+        //     chassis.turnToHeading(90, 2000);}
 
-         if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_X))  {
-            chassis.turnToHeading(0, 2000);
-            std::cout<<"SDGDSFSd";
+        //  if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_X))  {
+        //     chassis.turnToHeading(0, 2000);
+        //     std::cout<<"SDGDSFSd";
 
-         }
+        //  }
        
 
 
         //kiwiRunControl();
 
-        //matchControl();
+        matchControl();
 
         // firstPath.follow();
     }
